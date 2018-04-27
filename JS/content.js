1bcf93c2a6f506c14292c72f84cf7e3e90a74d51
Andy Gerstner <ajgerstner@fullsail.edu>
2018-04-27 12:27:33 -0400

    cleaned up code

--- JS/content.js
+++ JS/content.js
@@ -20,23 +20,22 @@ xhr.onload = function () {
         var responseObject = JSON.parse(xhr.responseText);
          
          
-var apiData='';
+         var apiData='';
 
       
      
-         apiData +='<ul class="apiSearch" id="apiSearchId">';
+            apiData +='<ul class="apiSearch" id="apiSearchId">';
         
-for( var i = 0; i < responseObject.results.length; i++)
-         {
+         for( var i = 0; i < responseObject.results.length; i++)
+        {
    
              
            
         
-  //            apiData +='<li>';
+  
             apiData +='<li><img src="'+responseObject.results[i].urls.small+'"';
             apiData +='srcset="'+responseObject.results[i].urls.thumb+' 200w,';
             apiData +=responseObject.results[i].urls.small+' 400w"';
-          //  apiData +=responseObject.results[i].urls.regular+' 800w"';
             apiData +='sizes="(max-width:300px) 80vw,';
             apiData +='(max-width:600px) 75vw, 275px"';
             apiData +='alt="'+responseObject.results[i].description+'" />';
@@ -46,46 +45,43 @@ for( var i = 0; i < responseObject.results.length; i++)
            
             
          }
- apiData +='</ul>';
+            apiData +='</ul>';
         
          if(document.getElementById('results')){
          //update the page with the new content
-      const listUpdated = document.getElementById('results');
-      listUpdated.innerHTML = apiData;
-      localStorage.setItem("searchEntered",apiData);
-        } }};
+             const listUpdated = document.getElementById('results');
+             listUpdated.innerHTML = apiData;
+                                                } }};
  
 
 
 
 
-//function to input search text
-window.onload =
-function () {
-    document.clear();
+            //function to input search text
+        window.onload =
+            function () {
  
-    
-xhr.open ('GET','https://api.unsplash.com/search/photos?random&per_page=24&query=mountain&client_id=e324c68611b84de22939d8f715a233ec7587e501a2164a618cb0990e3c07ad01',true);
-//send the request
-xhr.send(null); // no data needs to be sent to server
-   };
+            xhr.open ('GET','https://api.unsplash.com/search/photos?per_page=24&query=mountain&client_id=e324c68611b84de22939d8f715a233ec7587e501a2164a618cb0990e3c07ad01',true);
+            //send the request
+            xhr.send(null); // no data needs to be sent to server
+                        };
 
 
 
-    const object = document.getElementById("MountainLogo");
-    const back = object.getElementById("BackPeak");
-    const front = object.getElementById("FrontPeak");
+            const object = document.getElementById("MountainLogo");
+            const back = object.getElementById("BackPeak");
+            const front = object.getElementById("FrontPeak");
 
-document.getElementById("logo").addEventListener("click",jim,false);
-document.getElementById("logo").addEventListener("click",function(){
-    setTimeout(function(){back.setAttribute("class", "");
-    front.setAttribute("class","");},3000);
+            document.getElementById("logo").addEventListener("click",jim,false);
+            document.getElementById("logo").addEventListener("click",function(){
+            setTimeout(function(){back.setAttribute("class", "");
+            front.setAttribute("class","");},3000);
 
-},false);
+                },false);
 
-function jim(){
-    
-    back.setAttribute("class", "BackPeakAnim");
-    front.setAttribute("class","FrontPeakAnim");
-}
+            function jim(){
+
+            back.setAttribute("class", "BackPeakAnim");
+            front.setAttribute("class","FrontPeakAnim");
+                            }
    
